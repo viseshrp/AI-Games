0f854fb8393e8cfe66a38d7f2be403ab45d053bb
viseshrp <viseshrprasad@gmail.com>
2017-10-07 20:53:36 -0400

    Implement obstacle avoidance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,14 +21,23 @@ public class PlayerController : MonoBehaviour
 
 	Vector3 newPosition;
 
+	Vector3 towards;
+
 	public GameObject obstacle;
+
+	public int range;
 
 	// Use this for initialization
 	// called on the first frame when the script is active
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
-		maxSpeed = 10f;
+		maxSpeed = 10.0f;
+
+		speed = 10.0f;
+		range = 80;
+
+		obstacle.SetActive (false);
 	}
 
 	// Update is called once per frame and before rendering a frame, where most of our game code will go.
@@ -40,7 +49,7 @@ public class PlayerController : MonoBehaviour
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); //point camera in direction of mouse
 			if (Physics.Raycast (ray, out hit)) {
 				newPosition = hit.point; // set new/target position as mouse click point
-				Vector3 towards = newPosition - transform.position; // find the vector to take
+				towards = newPosition - transform.position; // find the vector to take
 				//Debug.Log(newPosition);
 				transform.rotation = Quaternion.LookRotation (towards, Vector3.right); //perform rotation towards click point
 
@@ -56,7 +65,9 @@ public class PlayerController : MonoBehaviour
 				//set velocity vector of rigid body
 				rb.velocity = towards;
 			}
+
 		}
+
 
 
 		// create obstacle on right click
@@ -64,6 +75,7 @@ public class PlayerController : MonoBehaviour
 			RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); //point camera in direction of mouse
 			if (Physics.Raycast (ray, out hit)) {
+				obstacle.SetActive (true);
 				GameObject obj = Instantiate (obstacle, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
 				obj.transform.position = new Vector3 (obj.transform.position.x, 0.5f, obj.transform.position.z);
 			}
@@ -74,6 +86,35 @@ public class PlayerController : MonoBehaviour
 		//if the ball is within a particular radius, stop it
 		if (rb.velocity.magnitude < satisfactionRadius) {
 			rb.velocity = Vector3.zero;
+		}
+
+		AvoidObstacle ();
+	}
+
+	void AvoidObstacle ()
+	{
+		//CHECKING AND AVOIDING OBSTACLES
+		RaycastHit hit2; //stores anything that is hit with the ray cast forward.
+		Ray checkingRay = new Ray (transform.position, Vector3.forward);
+		Debug.DrawRay (transform.position, Vector3.forward);
+		if (Physics.Raycast (checkingRay, out hit2, 3.3f)) {
+			if (hit2.transform != transform && hit2.collider.tag != "wall" && hit2.collider.tag != "ground" && hit2.collider.tag != "dudes") {
+				// avoid obstacle
+				Vector3 movement = new Vector3 (550.0f, 0.0f, 50.0f);
+				rb.AddForce (movement * speed);
+			}
+		}
+
+	}
+
+	//call on colliding
+	void OnCollisionEnter (Collision collision)
+	{
+		if (!collision.gameObject.CompareTag ("wall") && !collision.gameObject.CompareTag ("ground") && !collision.gameObject.CompareTag ("dudes")) { 
+			Debug.Log (collision.gameObject.name);
+			Vector3 movement = new Vector3 (300.0f, 0.0f, 0.0f);
+			rb.AddForce (movement * speed);
+
 		}
 	}
 }
--- Assets/Scripts/SecondaryPlayerController.cs
+++ Assets/Scripts/SecondaryPlayerController.cs
@@ -2,37 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SecondaryPlayerController : MonoBehaviour {
+public class SecondaryPlayerController : MonoBehaviour
+{
 
 	//reference to player to relate him with the camera
 	public GameObject player;
 
 	private Vector3 offset;
 
+	public float speed;
+	//public vars shows up in inspector as property
+	private Rigidbody rb;
+	//the rigid body to be attached to the sphere/object to perform physical actions on it.
+	private Transform target;
+	//holds static data for the target character
+
+	private float maxSpeed;
+	//max speed character can travel
+
+	public float satisfactionRadius = .1f;
+	//satisfaction radius
+
+	public float timeToTarget = 0.25f;
+
+	Vector3 newPosition;
+
+	Vector3 towards;
+
+	public GameObject obstacle;
+
+	public int range;
+
 	// Use this for initialization
-	void Start () {
-		offset = transform.position // position of camera
-			- player.transform.position; // position of player
+	// called on the first frame when the script is active
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		maxSpeed = 10.0f;
+
+		speed = 10.0f;
+		range = 80;
+
+		offset = transform.position// position of camera
+		- player.transform.position; // position of player
 	}
 
-	//	// Update is called once per frame
-	//	// Not appropriate or perfect for this purpose, so we switch to late update.
-	//	void Update () {
-	//		// make sure any movement in player is followed by movement in camera and offset/gap is maintained
-	//		// A.K.A Follow camera
-	//		transform.position = player.transform.position + offset;
-	//	}
+	void Update() {
+		transform.position = player.transform.position + offset;
+		AvoidObstacle ();
+	}
 
 	// Update Not appropriate or perfect for this purpose, so we switch to late update.
 	// Guaranteed to run after all items have been processed in Update(), i.e player has completely moved.
-	void LateUpdate () {
+	void LateUpdate ()
+	{
+		// make sure any movement in player is followed by movement in camera and offset/gap is maintained
+		// A.K.A Follow camera
 		transform.position = player.transform.position + offset;
+	}
 
-		// TODO: NEED TO DO SOME MATH TO GET INITIAL RELATIVE POSITIONS OF OFFSETTED BALLS
-		// CALCULATE A NEW DIRECTION/POSITION OF OFFSET AND APPLY IT ON BALLs.
-		// GET LIST OF ALL BALLS USING THIS SCRIPT before that.
-
+	void AvoidObstacle ()
+	{
+		//CHECKING AND AVOIDING OBSTACLES
+		RaycastHit hit2; //stores anything that is hit with the ray cast forward.
+		Ray checkingRay = new Ray (transform.position, Vector3.forward);
+		Debug.DrawRay (transform.position, Vector3.forward);
+		if (Physics.Raycast (checkingRay, out hit2, 3.3f)) {
+			// also, do not avoid if the object is a wall or ground or other balls
+			if (hit2.transform != transform && hit2.collider.tag != "wall" && hit2.collider.tag != "ground" && hit2.collider.tag != "dudes") {
+				//avoid obstacle
+				Vector3 movement = new Vector3 (150.0f, 0.0f, 0.0f);
+				rb.AddForce (movement * speed);
+			}
+		}
 
 	}
 
+	//called when entering
+	void OnCollisionEnter (Collision collision)
+	{
+		if (!collision.gameObject.CompareTag ("wall") && !collision.gameObject.CompareTag ("ground") && !collision.gameObject.CompareTag ("dudes")) { 
+			Debug.Log (collision.gameObject.tag);
+			Vector3 movement = new Vector3 (200.0f, 0.0f, 0.0f);
+			rb.AddForce (movement * speed);
+
+		}
+	}
+
+		
 }
